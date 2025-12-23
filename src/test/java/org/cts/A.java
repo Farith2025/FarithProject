3c4296f93f96252118da3ba3c068fea6888bcf0b
Farith2025 <nirmin.afn@gmail.com>
2025-12-23 23:06:44 +0530

    rehan code updated

--- src/test/java/org/cts/A.java
+++ src/test/java/org/cts/A.java
@@ -15,7 +15,6 @@ public class A {
 	username.sendKeys("Farith");
 	WebElement passbox = driver.findElement(By.id("pass"));
 	passbox.sendKeys("qwertyuio");
-	
 	driver.findElement(By.id("forgetpassword")).click();
 	
 	
