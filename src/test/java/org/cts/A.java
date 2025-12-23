1e7a70cfc4098956cce252df3c42b9eda00e503d
Farith2025 <nirmin.afn@gmail.com>
2025-12-23 21:59:30 +0530

    pushingmyprojectcodebrowserlaunch

---
+++ src/test/java/org/cts/A.java
@@ -0,0 +1,16 @@
+package org.cts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class A {
+	
+	public static void main(String[] args) {
+		
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://en-gb.facebook.com/");
+	
+	
+	}
+
+}
